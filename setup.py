06a5648e909e376ae266269f75a018b25aa6c8c1
xxMAKMAKxx <red.mak.up@gmail.com>
2018-11-20 20:29:46 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = ['vincent', 'folium', 'numpy', 'pandas', 'geocoder', 'paramik
 
 setup(name='plbmng',
       description='Tool for monitoring PlanetLab network',
-      version="0.2",
+      version="0.2.1",
       license='MIT',
       packages=find_packages(),
       package_data={},
